6db6679beabbd523695dfe2eff47ecd654fe8687
Anton Malofeev <arenukvern@gmail.com>
2023-01-14 20:27:37 +0200

    fix: models

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.0
+
+- added models export
+
 ## 0.1.0
 
 - Inital release.
--- lib/office_addin_helper.dart
+++ lib/office_addin_helper.dart
@@ -1,4 +1,5 @@
 library office_addin_helper;
 
 export './src/excel/excel.dart';
+export './src/models/models.dart';
 export './src/utils/utils.dart';
